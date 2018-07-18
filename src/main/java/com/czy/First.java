7e5cbcf2452eaf3463d9d6c9cc5540d6890461e6
Cheng Zhongyuan <cheng_zh@worksap.co.jp>
2018-07-18 10:25:40 +0800

    add unit test

--- src/main/java/com/czy/First.java
+++ src/main/java/com/czy/First.java
@@ -1,3 +1,5 @@
+package com.czy;
+
 public class First {
     public static void main(String[] args) {
         System.out.println("this is the first test");
---
+++ src/test/java/com/czy/FirstTest.java
@@ -0,0 +1,10 @@
+package com.czy;
+
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void test() {
+        System.out.println("this is a test class");
+    }
+}
